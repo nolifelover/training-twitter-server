f7ce5b57ba9c743928d50d1ec9c1977edbfd580b
todsaporn sangboon <nolifelover@gmail.com>
2018-07-22 12:23:41 +0700

    add twitter clone api

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require('express')
 const app = express()
+var tweets = [
+    {
+        id: 1,
+        name: "@nolifelover",
+        message: "hello world twitter 1 #hello #world",
+        created: "2018-07-22 12:07:20"
+    },
+    {
+        id: 2,
+        name: "@nolifelover",
+        message: "hello world twitter 2 #nodejs #memory",
+        created: "2018-07-22 12:10:20"
+    }
+]
 
 app.get('/', (req, res) => {
     let result = {
@@ -10,7 +24,35 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/tweets', (req, res) =>{
+    let result = {
+        status: 200,
+        datas: tweets
+    }
+    res.json(result)
+})
 
+app.post('/api/tweets', (req, res) =>{
+    let data = req.body
+    tweets.push(data)
+    let result = {
+        status: 200,
+        message: "success insert tweet"
+    }
+    res.json(result)
+})
+
+app.get('/api/search', (req, res) =>{
+    let q = req.query.q
+    let result = {
+        status: 200,
+        datas: []
+    }
+    tweets.forEach((tweet) =>{
+        if(tweet.message.indexOf(q) > -1){
+            result.datas.push(tweet)
+        }
+    })
+    res.json(result)
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
